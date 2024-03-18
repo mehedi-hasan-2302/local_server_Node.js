81cb3a9d7cc4314f5f8616f7723c28463b5b65eb
mehedi-hasan-2302 <mehedi.h2302@gmail.com>
2024-03-18 23:09:19 +0600

    css imported

--- app.js
+++ app.js
@@ -2,30 +2,38 @@ const http = require('http');
 const fs = require('fs');
 const port = 3000;
 
-
-const server = http.createServer(function(req, res){
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-
-    fs.readFile('index.html', function(error, data) {
-        if(error){
-            res.writeHead(404);
-            res.write('Error: File Not Found');
-        }
-        else{
-            res.write(data);
-        }
-        res.end();
-    })
-
+const server = http.createServer(function(req, res) {
+    if (req.url === '/style.css') {
+        // Serve the CSS file
+        fs.readFile('style.css', function(error, data) {
+            if (error) {
+                res.writeHead(404);
+                res.write('Error: File Not Found');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/css' });
+                res.write(data);
+            }
+            res.end();
+        });
+    } else {
+        // Serve the HTML file
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile('index.html', function(error, data) {
+            if (error) {
+                res.writeHead(404);
+                res.write('Error: File Not Found');
+            } else {
+                res.write(data);
+            }
+            res.end();
+        });
+    }
 });
 
 server.listen(port, function(error) {
-
-    if(error) {
-        console.log('Something went wrong',error);
-    }
-    else {
+    if (error) {
+        console.log('Something went wrong', error);
+    } else {
         console.log(`Server is listening on port ${port}`);
     }
-})
+});
